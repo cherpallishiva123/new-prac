9e83e58e00ea4aa16ba950987a6c4aebd14a463a
shiva cherpalli <cherpallishiva123@gmail.com>
2024-05-24 00:11:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+let username ='';
+while (username == "") 
+{
+    username = window.prompt('Your Name Please:')
+}
+    
+document.getElementById('sam').innerHTML = 'Hello! ' + " " + username 
+*/
